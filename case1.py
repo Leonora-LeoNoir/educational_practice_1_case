193184ac56d75847b179cde97c758bf4bb31baa7
Leonora Kolesova <kol.leo@list.ru>
2026-06-01 19:05:57 +0500

    Add function to get day of the wee

--- case1.py
+++ case1.py
@@ -1,8 +1,22 @@
-# -*- coding: utf-8 -*-
+from datetime import datetime
+
+def get_day_of_week(year, month, day): # Создаем объект даты
+    date_obj = datetime(year, month, day)
+    
+    # weekday() возвращает число от 0 (понедельник) до 6 (воскресенье)
+    days = [
+        "Понедельник", "Вторник", "Среда", 
+        "Четверг", "Пятница", "Суббота", "Воскресенье"
+    ]
+    
+    return days[date_obj.weekday()]
+
 print('Число вашего рождения (цифрами):') #текст подсказка для пользователя
-date = int(input()) #строка ввода
+day = int(input()) #строка ввода
 print('Месяц вашего рождения(цифрами):') #текст подсказка для пользователя
-mouth = int(input()) #строка ввода
+month = int(input()) #строка ввода
 print('Год вашего рождения(цифрами):') #текст подсказка для пользователя
 year = int(input()) #строка ввода
+
+print(get_day_of_week(year, month, day))
 
