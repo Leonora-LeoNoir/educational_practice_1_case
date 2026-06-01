f40a188c70dfb9be88cc556649fb65954ef0c035
Leonora Kolesova <kol.leo@list.ru>
2026-06-01 19:23:54 +0500

    Implement leap year check

--- case1.py
+++ case1.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import calendar
 
 def get_day_of_week(year, month, day): # Создаем объект даты
     date_obj = datetime(year, month, day)
@@ -13,10 +14,15 @@ def get_day_of_week(year, month, day): # Создаем объект даты
 
 print('Число вашего рождения (цифрами):') #текст подсказка для пользователя
 day = int(input()) #строка ввода
-print('Месяц вашего рождения(цифрами):') #текст подсказка для пользователя
+print('Месяц вашего рождения (цифрами):') #текст подсказка для пользователя
 month = int(input()) #строка ввода
-print('Год вашего рождения(цифрами):') #текст подсказка для пользователя
+print('Год вашего рождения (цифрами):') #текст подсказка для пользователя
 year = int(input()) #строка ввода
 
 print(get_day_of_week(year, month, day))
+
+if calendar.isleap(year):
+    print('Год високосный.')
+else:
+    print('Год не високосный.')
 
